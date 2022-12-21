3872bd574d045d276bf91b2cd2dd7e420df58679
elGerardo <gerardoyu54@gmail.com>
2022-12-20 23:51:04 -0800

    mysql connectimeout added

--- src/config/mysql.ts
+++ src/config/mysql.ts
@@ -5,7 +5,8 @@ const connection = mysql.createConnection({
     host: `${process.env.DB_HOST}`,
     user: `${process.env.DB_USERNAME}`,
     password: `${process.env.DB_PASSWORD}`,
-    database: `${process.env.DB_DATABASE}`
+    database: `${process.env.DB_DATABASE}`,
+    connectTimeout: 30000
 });
 
 const Connect = async () => {
