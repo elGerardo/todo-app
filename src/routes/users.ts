a124a048860f56075f4e56fe05b9cc0961cc197e
elGerardo <gerardoyu54@gmail.com>
2022-12-28 11:02:37 -0800

    x

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { register, login } from "../controllers/UsersController";
+import { register, login } from "../controllers/UsersController"
 
 const router = Router();
 
 router.post("/register", register);
 
-router.post("/login", login);
+router.post("/login", login)
 
-export { router };
+export { router }
