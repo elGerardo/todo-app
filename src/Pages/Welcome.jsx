3a389709a05c1d6583014dac3b5757afa3464f42
elGerardo <gerardoyu54@gmail.com>
2023-01-17 12:04:54 -0800

    landing page added

--- src/Pages/Welcome.jsx
+++ src/Pages/Welcome.jsx
@@ -1,9 +1,11 @@
 import style from "./Welcome.module.css";
 import buttons from "../assets/global/buttons.module.css";
-import { Container, Form, Spinner } from "react-bootstrap";
+import { Carousel, Container, Form, Spinner } from "react-bootstrap";
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { Users } from "../services/Users";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 
 //custom states
 let useField = ({ type, required }) => {
@@ -158,8 +160,8 @@ let SignUp = () => {
       }}
       exit={{ opacity: 0, x: -25 }}
     >
-      <div
-        className={`d-flex align-items-center justify-content-center w-100 bh-white my-5`}
+      <Container
+        className={`d-flex align-items-center justify-content-center bg-white my-5`}
       >
         <div>
           <Form onSubmit={handleSubmit} style={{ textAlign: "left" }}>
@@ -200,65 +202,149 @@ let SignUp = () => {
             </button>
           </Form>
         </div>
-      </div>
+      </Container>
     </motion.div>
   );
 
   return content;
 };
 
+let Examples = () => {
+  let content = (
+    <div
+      className={`${style.container_examples} position-relative d-flex align-items-center justify-content-around flex-column bg-dark text-white text-center w-100 pt-3`}
+    >
+      <a
+        href="#examples"
+        className={`position-absolute text-center top-0 mt-2`}
+      >
+        <h1>See how this works</h1>
+        <FontAwesomeIcon icon={faArrowDown} />
+      </a>
+      <Container className={`mt-5`}>
+        <Carousel variant="dark" id="examples">
+          <Carousel.Item>
+            <img
+              src="./examples/example_1.png"
+              alt="First Example"
+              className={`w-100`}
+            />
+            <Carousel.Caption className={`mt-5 bg-dark text-white rounded`}>
+              <h1>ToDo list</h1>
+              <p>Organizing your activities creating a ToDo list</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              src="./examples/example_2.png"
+              alt="Second Example"
+              className={`w-100`}
+            />
+            <Carousel.Caption className={`bg-dark text-white rounded`}>
+              <h1>Progress</h1>
+              <p>Keep the progress of your tasks</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              src="./examples/example_3.png"
+              alt="Third Example"
+              className={`w-100`}
+            />
+            <Carousel.Caption className={`bg-dark text-white rounded`}>
+              <h1>Dinamyc dashboard</h1>
+              <p>By grid or rows, your decide how to see your note cards</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </Container>
+    </div>
+  );
+
+  return content;
+};
+
+let ContainerLogin = () => {
+  let [isLogin, setIsLogin] = useState(true);
+
+  let content = (
+    <motion.div
+      key="welcome"
+      initial={{ opacity: 0, y: -25 }}
+      transition={{ y: { duration: 0.25 } }}
+      animate={{
+        y: 0,
+        opacity: 1,
+        transition: {
+          duration: 0.25,
+        },
+      }}
+      exit={{ opacity: 0, y: 25 }}
+    >
+      <h1 className={`text-white w-100 text-center`}>ToDo App</h1>
+      <div
+        className={`bg-white text-center shadow-lg rounded pt-5 pe-5 ps-5 pb-3`}
+        style={{ height: "auto" }}
+      >
+        <div>
+          <button
+            onClick={() => setIsLogin(true)}
+            className={`${
+              isLogin ? buttons.primary_selected : buttons.primary
+            } mx-3 ${isLogin ? "shadow" : ""}`}
+          >
+            Login
+          </button>
+          <button
+            onClick={() => setIsLogin(false)}
+            className={`${
+              !isLogin ? buttons.primary_selected : buttons.primary
+            } mx-3 ${!isLogin ? "shadow" : ""}`}
+          >
+            Create Account
+          </button>
+        </div>
+        {isLogin ? <Login /> : <SignUp />}
+      </div>
+    </motion.div>
+  );
+  return content;
+};
+
 //page
 let Welcome = () => {
-  let [isLogin, setIsLogin] = useState(true);
+  let [hasStarted, setHasStarted] = useState(false);
 
   useEffect(() => {
     localStorage.removeItem("login");
   }, []);
 
   let content = (
-    <div
-      className={`${style.container} d-flex align-items-center justify-content-center text-center my-5`}
-    >
-      <div className={`${style.bg_welcome}`}></div>
-      <motion.div
-        key="welcome"
-        initial={{ opacity: 0, y: -200 }}
-        transition={{ y: { duration: 0.5 } }}
-        animate={{
-          y: 0,
-          opacity: 1,
-          transition: {
-            duration: 0.5,
-            delay: 0.5,
-          },
-        }}
-        exit={{ opacity: 0, y: 200 }}
+    <div>
+      <div
+        className={`${style.container} w-100 d-flex align-items-center justify-content-around`}
       >
-        <h1 className={`text-white`}>ToDo App</h1>
-        <div
-          className={`${style.content} bg-white shadow-lg rounded pt-5 pe-5 ps-5 pb-3`}
-        >
-          <div>
+        <div className={`${style.bg_welcome}`}></div>
+        {!hasStarted && (
+          <div className={`${style.info_content} text-white text-center p-5`}>
+            <h1>ToDo App</h1>
+            <p>
+              A place where you can keep a life style organized, creating notes
+              and task list that always help you to never forgot something.
+            </p>
             <button
-              onClick={() => setIsLogin(true)}
-              className={`${
-                isLogin ? buttons.primary_selected : buttons.primary
-              } mx-3 ${isLogin ? "shadow" : ""}`}
+              className={`${buttons.secondary} shadow-lg`}
+              onClick={() => {
+                setHasStarted(true);
+              }}
             >
-              Login
-            </button>
-            <button
-              onClick={() => setIsLogin(false)}
-              className={`${
-                !isLogin ? buttons.primary_selected : buttons.primary
-              } mx-3 ${!isLogin ? "shadow" : ""}`}
-            >
-              Create Account
+              Get Started
             </button>
           </div>
-          {isLogin ? <Login /> : <SignUp />}
-        </div>
-      </motion.div>
+        )}
+        {hasStarted && <ContainerLogin />}
+      </div>
+      <Examples />
     </div>
   );
 
